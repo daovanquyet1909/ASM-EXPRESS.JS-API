f5af8ca2581c1458237b927c6d0b50ac85ab5d56
Admin <160482818+daovanquyet1909@users.noreply.github.com>
2024-02-19 18:52:30 +0700

    .

---
+++ src/config/cloudinaryConfig.js
@@ -0,0 +1,9 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dnkf9xofi', 
+  api_key: '961985579319626', 
+  api_secret: 'Nx_QTV4_G0Slbx9CiMG2scP309s' 
+});
+
+export default cloudinary;
